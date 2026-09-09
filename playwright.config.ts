8712f3bb4c0e4edcf87d84770c1b04500290f3e6
nandha2532 <vnandhakumar19101999@gmail.com>
2026-09-09 09:59:10 +0530

    modify config for azure html report

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [['html'],['github']],
+  // reporter: [['html'],['github']], //for github actions and jenkins
+  reporter:[['html',{open:'never'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
